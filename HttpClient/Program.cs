b27168762cc95416a755d137d65b5f4d6e5431de
Ievgenii Iukhymchuk <ievgenii_iukhymchuk@epam.com>
2017-02-02 15:27:46 +0200

    Move request building logic into separate file

--- HttpClient/Program.cs
+++ HttpClient/Program.cs
@@ -10,60 +10,10 @@ namespace HttpClient
 {
 	class Program
 	{
-		const string NuanceHostName = "dictation.nuancemobility.net";
-		const string NuancePath = "/NMDPAsrCmdServlet/dictation";
-
-		static Uri BuildUri()
-		{
-			var uriBuilder = new UriBuilder();
-			uriBuilder.Scheme = Uri.UriSchemeHttps;
-			uriBuilder.Host = NuanceHostName;
-			uriBuilder.Path = NuancePath;
-			uriBuilder.Query = BuildUrlQuery(new { appId = "MY_APP", appKey = "777", id = "111" });
-			return uriBuilder.Uri;
-		}
-
-		static string BuildUrlQuery(object values)
-		{
-			var type = values.GetType();
-			var properties = type.GetProperties();
-			var resultBuilder = new StringBuilder(32);
-			foreach (var property in properties)
-			{
-				var key = property.Name;
-				var value = type.GetProperty(key)?.GetValue(values).ToString();
-
-				resultBuilder.Append(HttpUtility.UrlEncode(key));
-				resultBuilder.Append("=");
-				resultBuilder.Append(HttpUtility.UrlEncode(value));
-				resultBuilder.Append("&");
-			}
-			resultBuilder.Length--; // delete trailing '&'
-
-			return resultBuilder.ToString();
-		}
-
 		static void Main(string[] args)
 		{
-			var url = BuildUri();
-			var requestMethod = "POST";
-			var requestContentType = "application/octet-stream";
-			var isChunked = true;
-			var allowWriteStreamBuffer = false;
-			var requestUserAgent = "mine";
-			var requestAcceptHeader = "text/plain";
-			var AcceptLanguage = "en-US";
-
-			var request = (HttpWebRequest)WebRequest.Create(url);
-			request.Method = requestMethod;
-			request.AllowWriteStreamBuffering = allowWriteStreamBuffer;
-			request.SendChunked = isChunked;
-
-			request.ContentType = requestContentType;
-			request.UserAgent = requestUserAgent;
-			request.Accept = requestAcceptHeader;
-			request.Headers.Add($"Accept-Language: {AcceptLanguage}");
-
+			var request = new HttpWebRequestBuilder().Build();
+			// chunk size - 4160 bytes  of PCM 16 bits 8 kHz
 			var stream = request.GetRequestStream();
 
 			string nextLine;
